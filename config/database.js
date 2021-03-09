10c9a5f2f6f9a1273022b9d3bb6ad7d6c6e3c869
unknown <deepak@sugoilabs.com>
2021-03-09 19:07:25 +0530

    products

--- config/database.js
+++ config/database.js
@@ -3,9 +3,6 @@ const Sequelize = require('sequelize');
 module.exports= new Sequelize('DataBase','postgres','Deep@k0506',{
     host:'localhost',
     dialect:'postgres',
-    user:'postgres',
-    password:'Deep@k0506',
-    database:'DataBase',
     port:'5432'
 });
 //test db
